d846b579a88208af10bd6b66c4a02c6ee794b068
Hamid2158 <hamidnaeem7986@gmail.com>
2026-06-08 17:27:51 +0500

    fix dotenv

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from dotenv import load_dotenv
+
 import os
 import streamlit as st
 from groq import Groq
 from datetime import datetime
 import sqlite3
-
-load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def init_db():
